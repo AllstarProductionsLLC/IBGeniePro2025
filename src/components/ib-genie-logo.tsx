6fb9534b9dd2968e98b6b0903109bb1deb7bc84c
Allstar Productions <info@achievementwithai.com>
2025-08-25 00:37:46 +0000

    Now there is no open or close section button? Please add one also change

--- src/components/ib-genie-logo.tsx
+++ src/components/ib-genie-logo.tsx
@@ -4,6 +4,8 @@ export function IbGenieLogo(props: SVGProps<SVGSVGElement>) {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
       viewBox="0 0 24 24"
       fill="none"
       stroke="currentColor"
@@ -15,9 +17,9 @@ export function IbGenieLogo(props: SVGProps<SVGSVGElement>) {
       <title>IBGenie Logo</title>
       <path d="M12 2a10 10 0 1 0 10 10" />
       <path d="M12 2a10 10 0 1 0-1.41-1.41" />
+      <path d="m15.5 6.5 3 3" />
+      <path d="m8.5 17.5 3 3" />
       <path d="M12 8a4 4 0 0 0-4 4h8a4 4 0 0 0-4-4Z" />
-      <path d="M10 12h.01" />
-      <path d="M14 12h.01" />
     </svg>
   );
 }
